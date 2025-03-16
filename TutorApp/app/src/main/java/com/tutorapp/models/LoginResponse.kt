1006388c065c95b565f8ad87200afb103e28e681
EDA2021-1-SEC04-G01 <j.riverao@uniandes.edu.co>
2025-03-16 12:08:26 -0500

    Added Login and register vieews, Fully functional and also already connected to backend

---
+++ TutorApp/app/src/main/java/com/tutorapp/models/LoginResponse.kt
@@ -0,0 +1,6 @@
+package com.tutorapp.models
+
+data class LoginResponse(
+    val token: String,
+    val userId: String
+)
--- TutorApp/app/src/main/java/com/tutorapp/ui/theme/Color.kt
+++ TutorApp/app/src/main/java/com/tutorapp/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.tutorapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0xFF192650)
+val Secondary = Color(0xFF29339B)
+val Black = Color(0x00000000)
+val White = Color(0xFFFFFFFF)
+val LightGrey = Color(0xFF49454E)
