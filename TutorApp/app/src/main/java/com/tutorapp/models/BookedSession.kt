b09e87f5f1e1667b1ac34a80e73f0e2eadba5360
Jonathan Rivera <j.riverao@uniandes.edu.co>
2025-05-22 15:07:13 -0500

    Fixed error whith bookedsession object handling

--- TutorApp/app/src/main/java/com/tutorapp/models/BookedSession.kt
+++ TutorApp/app/src/main/java/com/tutorapp/models/BookedSession.kt
@@ -1,5 +1,9 @@
 package com.tutorapp.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class BookedSession(
     val id: Int,
     val tutorName: String,
@@ -7,7 +11,7 @@ data class BookedSession(
     val cost: String,
     val dateTime: String,
     val student: String
-)
+) : Parcelable
 
 data class BookedSessionsResponse(
     val booked_sessions: List<BookedSession>
